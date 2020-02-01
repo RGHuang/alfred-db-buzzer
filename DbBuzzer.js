e2d6ed3a614a3c5fb96b4a277a62ab1063b2cfa5
RGHuang <ziwei443@gmail.com>
2020-02-01 18:31:41 +0800

    database buzzer ver0.0.1

---
+++ DbBuzzer.js
@@ -0,0 +1,29 @@
+const slackAPI = require('slackbots');
+require('dotenv').config();
+
+const targetChannel = 'buzzer_alfred';
+
+const slackBot =
+    new slackAPI({
+        token: `${process.env.SLACK_TOKEN}`,
+        name: 'ALFRED'
+    })
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/alfred', { useUnifiedTopology: true, useNewUrlParser: true }).then(() => console.log('DB Connected!'))
+    .catch(err => {
+        console.log(err);
+    });
+
+slackBot.on('message', () => {
+    if (mongoose.connection.readyState == 0) {
+        slackBot.postMessageToChannel(targetChannel, 'Database disconnected');
+    } else if (mongoose.connection.readyState == 2) {
+        slackBot.postMessageToChannel(targetChannel, 'Database is connecting');
+    } else if (mongoose.connection.readyState == 3) {
+        slackBot.postMessageToChannel(targetChannel, 'Database is disconnecting');
+    } else if (mongoose.connection.readyState == 1) {
+        console.log('DB is connected');
+    }
+})
+
