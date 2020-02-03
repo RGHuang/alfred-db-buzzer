daab1a540eeb3c28ba3af2d8d3e59ff187c414fe
RGHuang <ziwei443@gmail.com>
2020-02-03 09:00:48 +0800

    Transfer mlab to mongoDB altas(common db for Alfred)

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 const slackAPI = require('slackbots');
 require('dotenv').config();
+var CronJob = require('cron').CronJob;
 
 const targetChannel = 'buzzer_alfred';
 
@@ -15,16 +16,26 @@ mongoose.connect(`${process.env.MONGODB_URL}`, { useNewUrlParser: true }).then((
         console.log(err);
     });
 
+let checkStatusCron = new CronJob('* 2 * * * * ', function () {
+    console.log('start checking');
+    sendWarningToSlack();
+}, null, true, 'America/Los_Angeles');
+checkStatusCron.start();
 
-slackBot.on('message', () => {
-    if (mongoose.connection.readyState == 0) {
-        slackBot.postMessageToChannel(targetChannel, 'Database disconnected');
-    } else if (mongoose.connection.readyState == 2) {
-        slackBot.postMessageToChannel(targetChannel, 'Database is connecting');
-    } else if (mongoose.connection.readyState == 3) {
-        slackBot.postMessageToChannel(targetChannel, 'Database is disconnecting');
-    } else if (mongoose.connection.readyState == 1) {
-        console.log('DB is connected');
-    }
-})
+
+
+function sendWarningToSlack() {
+    slackBot.on('message', () => {
+        if (mongoose.connection.readyState == 0) {
+            slackBot.postMessageToChannel(targetChannel, 'Database disconnected');
+        } else if (mongoose.connection.readyState == 2) {
+            slackBot.postMessageToChannel(targetChannel, 'Database is connecting');
+        } else if (mongoose.connection.readyState == 3) {
+            slackBot.postMessageToChannel(targetChannel, 'Database is disconnecting');
+        } else if (mongoose.connection.readyState == 1) {
+            console.log('DB is connected');
+        }
+    })
+}
+
 
