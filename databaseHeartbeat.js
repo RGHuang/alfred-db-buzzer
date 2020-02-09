b5b9cee5eaa98bc54fd10876c05799df3be534ca
RGHuang <ziwei443@gmail.com>
2020-02-09 20:39:01 +0800

    Revise cronJob -> schecduleJob to confirm heartbeat work correctly

---
+++ databaseHeartbeat.js
@@ -0,0 +1,40 @@
+const slackAPI = require('slackbots');
+require('dotenv').config();
+let schedule = require('node-schedule');
+
+
+const targetChannel = 'buzzer_alfred';
+
+const slackBot =
+    new slackAPI({
+        token: `${process.env.SLACK_TOKEN}`,
+        name: 'ALFRED_Database_Heartbeat'
+    })
+
+let mongoose = require('mongoose');
+mongoose.connect(`${process.env.BOT_MONGODB_URL}`, { useNewUrlParser: true }).then(() => console.log('DB Connected!'))
+    .catch(err => {
+        console.log(err);
+    });
+
+let checkStatusSchedule = new schedule.scheduleJob('10 * * * * * ', function () {
+    console.log('start checking');
+    let checkingTime = new Date();
+    sendWarningToSlack(checkingTime);
+});
+
+
+
+function sendWarningToSlack(time) {
+    if (mongoose.connection.readyState == 0) {
+        slackBot.postMessageToChannel(targetChannel, 'Database disconnected at ' + time);
+    } else if (mongoose.connection.readyState == 2) {
+        slackBot.postMessageToChannel(targetChannel, 'Database is connecting at ' + time);
+    } else if (mongoose.connection.readyState == 3) {
+        slackBot.postMessageToChannel(targetChannel, 'Database is disconnecting at ' + time);
+    } else if (mongoose.connection.readyState == 1) {
+        console.log('DB is connected at ' + time);
+    }
+}
+
+
